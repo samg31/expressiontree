60e9e6efb84913d5c3fdd85994ee8fc5f2f8a4a2
sam <sam@localhost.localdomain>
2017-01-20 17:03:53 -0500

    implemented virtual evaluate function

--- expr.hpp
+++ expr.hpp
@@ -1,31 +1,54 @@
+#include <memory>
+
 class Expr
 {
 public:
+    virtual bool Evaluate() = 0;
     virtual ~Expr();
 };
 
 class TrueExpr : public Expr
 {
+    bool Evaluate(){ return true; }
 };
 
 class FalseExpr : public Expr
 {
+    bool Evaluate(){ return false; }
 };
-
 
 class AndExpr : public Expr
 {
     Expr* e1;
     Expr* e2;
+public:
+    AndExpr( Expr* lhs, Expr* rhs );
+
+    bool Evaluate()
+    {
+	return ( e1->Evaluate() && e2->Evaluate() );
+    }
 };
 
 class OrExpr : public Expr
 {
     Expr* e1;
     Expr* e2;
+
+    bool Evaluate()
+    {
+	return ( e1->Evaluate() && e2->Evaluate() );
+    }
 };
 
 class NotExpr : public Expr
 {
     Expr* e;
+public:
+    NotExpr( Expr* ex );
+
+    bool Evaluate()
+    {
+	return ( !e->Evaluate() );
+    }
 };
--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include "expr.hpp"
 
+#include <memory>
+#include <iostream>
+
 int main()
 {
     Expr* e1 = new TrueExpr;
-    Expr* e2 = new FalseExpr;
+    Expr* e2 = new TrueExpr;
     Expr* e3 = new AndExpr( e1, e2 );
 
-    delete e1;
-    delete e2;
-    delete e3;
-
+    std::cout << e3->Evaluate() << '\n';
     return 0;
 }
