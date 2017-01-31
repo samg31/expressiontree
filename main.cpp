3b14cc1cdae101161c450e6c6cb9c178b3efe39a
sam <sdg31@knuth2.cs.uakron.edu>
2017-01-31 15:02:16 -0500

    updated to fit with astcontext

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 #include "expr.hpp"
+#include "astcontext.hpp"
 
 #include <memory>
 #include <iostream>
 
 int main()
 {
+    ASTContext context;
+
     Expr* e1 = new TrueExpr;
     Expr* e2 = new TrueExpr;
     Expr* e3 = new AndExpr( e1, e2 );
 
-    std::cout << e3->Evaluate() << '\n';
+    std::cout << e3->Evaluate( context ) << '\n';
     return 0;
 }
